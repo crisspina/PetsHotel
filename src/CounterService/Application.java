dac9c8bf2295eef2bb30c220a9ef84a2e02cbb2a
crisspina <nutchapannoi@gmail.com>
2020-04-29 00:51:56 +0700

    [ADD] add Application
    ยังไม่เสร็จ

--- src/CounterService/Application.java
+++ src/CounterService/Application.java
@@ -6,12 +6,25 @@ import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
-        String menu = "Select Menu: \n "
-                + "1. Add Customer \n "
-                + "2. List all Customer \n"
-                + "0. Exit \n"
-                + "-----Select: ";
         
-        Scanner sc = new Scanner(System.in);
         
+//          // TODO code application logic here
+//        RoomServiceController controller = new RoomServiceController();
+//        // Start the application
+//        controller.startApplication();
+//        
+//        
+        
+        
+//    }
+//        String menu = "Select Menu: \n "
+//                + "1. Add Customer \n "
+//                + "2. List all Customer \n"
+//                + "0. Exit \n"
+//                + "-----Select: ";
+//        
+//        Scanner sc = new Scanner(System.in);
+//        
+
+    }
 }
