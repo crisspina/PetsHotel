d0a85f4853a2939b9917876646be22128a459f6a
crisspina <nutchapannoi@gmail.com>
2020-04-29 00:45:15 +0700

    [UPDATE] edit customer class

--- src/Customers/Customers.java
+++ src/Customers/Customers.java
@@ -4,19 +4,24 @@ package Customers;
 import java.util.Objects;
 
 public class Customers {
+    private int CustomerID;
     private String fName;
     private String lName;
     private String phoneNumber;
     private Pet pet;
- 
 
-    public Customers(String fName, String lName, String phoneNumber, Pet pet) {
+    public Customers(int CustomerID, String fName, String lName, String phoneNumber, Pet pet) {
+        this.CustomerID = CustomerID;
         this.fName = fName;
         this.lName = lName;
         this.phoneNumber = phoneNumber;
         this.pet = pet;
     }
 
+    public int getCustomerID() {
+        return CustomerID;
+    }
+    
     public String getfName() {
         return fName;
     }
@@ -32,6 +37,28 @@ public class Customers {
     public Pet getPet() {
         return pet;
     }
+
+    public void setCustomerID(int CustomerID) {
+        this.CustomerID = CustomerID;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+    
+    
 
     @Override
     public int hashCode() {
