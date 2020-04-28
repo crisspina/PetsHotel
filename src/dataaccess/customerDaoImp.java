52c628f039593e964cce6c09744c166208375a2f
crisspina <nutchapannoi@gmail.com>
2020-04-29 00:50:52 +0700

    [ADD] CustomerDaoImp
    ยังไม่เสร็จ

--- src/dataaccess/customerDaoImp.java
+++ src/dataaccess/customerDaoImp.java
@@ -1,82 +1,76 @@
 
 package dataaccess;
 
+import Customers.Customers;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import model.Customer;
-import model.GeneralList;
-import model.dao.CustomerDao;
 
 
+public class CustomerDaoImp implements CustomerDao {
 
-public class customerDaoImp {
-    
-public class CustomerDaoImp implements CustomerDao{
-
+    private final Connection conn = DBConnection.getConnection();
+    private final String SQL_CREATE_CUSTOMER = "INSERT INTO customer (fName, lName,phoneNumber,name) VALUES ( ?, ?, ?, ?)";
+    private final String SQL_GET_ALL_CUSTOMER = "SELECT * FROM customer";
+    private final String SQL_GET_CUSTOMER_BY_PETNAME = "SELECT * FROM name=?";   //name is petname
+    private final String SQL_UPDATE_CUSTOMER = "UPDATE customer SET fName=?, lName=?, phoneNumber=? WHERE name=?";  //ยังไม่ชัวร์เดี๋ยวดูอีกที
+    private final String SQL_DELETE_CUSTOMER = "DELETE FROM customer WHERE fName=? AND name=? ";
     @Override
-    public int insert(Customer obj) {
-        int rows=0;
-        String sql = "INSERT INTO CUSTOMER VALUES(?,?)";
-        
-        try(Connection con = DBConnection.getConnection();
-               
-                PreparedStatement pstm = con.prepareStatement(sql)){
-            pstm.setInt(1, obj.getCus_id());
-            pstm.setString(2, obj.getCus_name());
-            pstm.executeUpdate();
-        
-        }catch(SQLException ex){
-            System.out.println(ex);
-        }
-        
-        return rows;
-    }
-
-    @Override
-    public int delete(Customer obj) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-
-    }
-
-    @Override
-    public int update(Customer obj) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public Customer findById(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public GeneralList<Customer> findByName(String name) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public GeneralList<Customer> getAll() {
-        
-        GeneralList<Customer> cus = new GeneralList<Customer>();
-        try(Connection con = DBConnection.getConnection();
-                Statement stm = con.createStatement()){
-            
-            ResultSet rs = stm.executeQuery("SELECT * FROM CUSTOMER");
-            while(rs.next()){
-                cus.add(new Customer(rs.getInt("cus_id"),rs.getString("cus_name")));
+    public int createCustomer(Customers obj) {
+        int row = 0;
+        try (PreparedStatement pstmt = conn.prepareStatement(SQL_CREATE_CUSTOMER, Statement.RETURN_GENERATED_KEYS)) 
+            pstmt.setString(1, obj.getfName());
+            pstmt.setString(2, obj.getlName());
+            pstmt.setString(3, obj.getPhoneNumber());
+            pstmt.setString(4, obj.getPet());
+            row = pstmt.executeUpdate();
+            try (ResultSet generatedKeys = pstmt.getGeneratedKeys()) {
+                if (generatedKeys.next()) {
+                    customer.setCustomerID(generatedKeys.getInt(1));
+                }
             }
-            
         } catch (SQLException ex) {
-            Logger.getLogger(CustomerDaoImp.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(CustomerDaoImpl.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
-        return cus;
+        return row;
+    }
+
+    @Override
+    public int updateCustomer(Customers obj) {
+       int row = 0;
+        try (PreparedStatement pstmt = conn.prepareStatement(SQL_UPDATE_CUSTOMER)) {
+            pstmt.setString(1, customer.getFirstname());
+            pstmt.setString(2, customer.getLastname());
+            pstmt.setString(3, customer.getEmail());
+            pstmt.setInt(4, customer.getAge());
+            pstmt.setString(5, customer.getPhone());
+            row = pstmt.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(CustomerDaoImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return row;
+    }
+
+    @Override
+    public int deleteCustomer(Customers obj) {
+         int row = 0;
+        try (PreparedStatement pstmt = conn.prepareStatement(SQL_DELETE_CUSTOMER)) {
+            pstmt.setInt(1, customerId);
+            row = pstmt.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(CustomerDaoImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return row;
+    }
+
+    @Override
+    public ArrayList<Customers> getAllCustomer() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
-}
-
 }
