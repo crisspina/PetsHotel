86880f76e12a8f96f5aa88c5f8c4f75454fb4cdf
giftsnt <sunanta.gift@mail.kmutt.ac.th>
2020-04-30 01:03:03 +0700

    ReserCus

--- src/Customers/ReservedCustomers.java
+++ src/Customers/ReservedCustomers.java
@@ -4,15 +4,19 @@ package Customers;
 import Activities.Activities;
 import CounterService.ReservedStatus;
 import Room.RoomType;
+import java.util.Scanner;
 
 public class ReservedCustomers  {
     private RoomType resRoom;
-    private Activities resAct[];
+    private Activities resAct;
     private final int numberOfActivities=6;
     private Customers customer;
     private ReservedStatus status;
     private int amount;
 
+    public ReservedCustomers(){
+        
+    }
     public ReservedCustomers(RoomType resRoom, Customers customer) {
         this.resRoom = resRoom;
         this.customer = customer;
@@ -20,10 +24,40 @@ public class ReservedCustomers  {
     }
     
     public void setReservedActivities(){
-        this.resAct= new Activities[this.numberOfActivities];
-        /*resAct[1]=Activities.EXERCISE;
-        resAct[2]=Activities.MESSAGEANDSPA;
-        กิ๊ฟมาทำ*/
+        Scanner sn = new Scanner(System.in);
+        
+        int choice;
+            System.out.println("Activities: ");
+            System.out.println("\t 1. GROOMING");
+            System.out.println("\t 2. PLAYTIME");
+            System.out.println("\t 3. GARDEN");
+            System.out.println("\t 4. EXERCISE");
+            System.out.println("\t 5. MESSAGEANDSPA");
+            System.out.println("\t 6. PHOTOSET");
+            System.out.print("Enter your number activities: ");
+            choice = sn.nextInt();
+            switch (choice){
+                case 1:
+                    resAct = Activities.GROOMING;
+                    break;
+                case 2:
+                    resAct = Activities.PLAYTIME;
+                    break;
+                case 3:
+                    resAct = Activities.GARDEN;
+                    break;
+                case 4:
+                    resAct = Activities.EXERCISE;
+                    break;
+                case 5:
+                    resAct = Activities.MESSAGEANDSPA;
+                    break;
+                case 6:
+                    resAct = Activities.PHOTOSET;
+                    break;
+            }
+        System.out.println("-------------------");
+        System.out.println("Activities: " + resAct);
     }
  
     public RoomType getResRoom() {
@@ -31,10 +65,12 @@ public class ReservedCustomers  {
     }
 
     public Activities[] getResAct() {
-        for(Activities resActs: resAct)
-            System.out.println(resActs);
-        return resAct;
+        return null;
+//        for(Activities resActs: resAct)
+//            System.out.println(resActs);
+//        return resAct;
     }
+    
 
     public Customers getCustomers() {
         return customer;
--- src/Customers/Test.java
+++ src/Customers/Test.java
@@ -5,7 +5,9 @@ public class Test {
     public static void main(String[] args) {
 //        Pet p = new Pet();
 //        p.petScan();
-        Customers c = new Customers();
-        c.cusScan();
+//        Customers c = new Customers();
+//        c.cusScan();
+        ReservedCustomers rc = new ReservedCustomers();
+        rc.setReservedActivities();
     }
 }
